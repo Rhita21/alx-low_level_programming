6096ec9233668e4ec3ff4c135d308dc6a8afc06f
Rhita21 <achchak.rhita@gmail.com>
2023-03-22 17:33:14 -0700

    print last digit numer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,16 +11,10 @@ int print_last_digit(int n)
 {
 int last_digit;
 
-/* Compute the absolute value of n */
 if (n < 0)
 n = -n;
 
-/* Extract the last digit of n */
 last_digit = n % 10;
-
-/* Print the last digit of n */
 _putchar('0' + last_digit);
-
-/* Return the value of the last digit */
 return (last_digit);
 }
